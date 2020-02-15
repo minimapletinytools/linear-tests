7feb7995e4401670140fe18b865b0df76f8c03be
pddla <chippermonky@gmail.com>
2020-02-14 17:15:02 -0800

    switched V3 tests to use TypeApplication instead of Double, also fixed an issue with signorm test

--- test/Linear/V3Spec.hs
+++ test/Linear/V3Spec.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ExplicitForAll      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+
+
 module Linear.V3Spec (
   spec
 ) where
@@ -12,28 +18,32 @@ import           Linear.Vector
 import           Linear.V3.Arbitrary
 
 import           Debug.Trace
+
 -- | test for the property `|v| = 1`
-prop_UnitV3_isUnit :: UnitV3 Double -> Bool
+prop_UnitV3_isUnit :: (Epsilon a, Floating a) => UnitV3 a -> Bool
 prop_UnitV3_isUnit (UnitV3 v) = nearZero $ norm v - 1
 
 -- | test for the property `|v*c| = c` for unit vector v and arbitrary scalar c
-prop_Metric_V3_norm :: UnitV3 Double -> Double -> Bool
+prop_Metric_V3_norm :: (Epsilon a, Floating a) => UnitV3 a -> a -> Bool
 prop_Metric_V3_norm (UnitV3 v) c = nearZero $ norm (v ^* c) - (abs c)
 
--- | test for the property `signorm(v*c) = v` for unit vector v
-prop_Metric_V3_signorm :: UnitV3 Double -> Double -> Bool
-prop_Metric_V3_signorm (UnitV3 v) c = nearZero $ signorm (v ^* c) - v
+-- | test for the property `signorm(v*c) = v` for unit vector v and c >= 0
+prop_Metric_V3_signorm :: (Epsilon a, Floating a) => UnitV3 a -> NonZero a -> Bool
+prop_Metric_V3_signorm (UnitV3 v) c = nearZero $ signorm (v ^* (abs (getNonZero c))) - v
 
 
 spec :: Spec
-spec = do
+spec = specTyped @Double
+
+specTyped :: forall a. (Eq a, Show a, Arbitrary a, Epsilon a, Floating a) => Spec
+specTyped = do
   describe "V3" $ do
     describe "Arbitrary" $ do
       describe "UnitV3" $ do
         it "satisfies the property `|v| = 1`" $ do
-          property prop_UnitV3_isUnit
+          property $ prop_UnitV3_isUnit @a
     describe "Metric" $ do
       it "satisfies property `|v*c| = c` for unit vectors" $
-        property prop_Metric_V3_norm
+        property $ prop_Metric_V3_norm @a
       it "satisfies property `signorm(v*c) = v` for unit vectors" $
-        property prop_Metric_V3_norm
+        property $ prop_Metric_V3_signorm @a
